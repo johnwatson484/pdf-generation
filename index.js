acdf62f6fd45a1f8c9465b7890557b6dd0e3386d
John Watson <johnwatson484@hotmail.com>
2022-07-22 17:29:36 +0100

    Basic PDF generation added

---
+++ index.js
@@ -0,0 +1,23 @@
+const fonts = {
+  Roboto: {
+    normal: 'fonts/Roboto-Regular.ttf',
+    bold: 'fonts/Roboto-Medium.ttf',
+    italics: 'fonts/Roboto-Italic.ttf',
+    bolditalics: 'fonts/Roboto-MediumItalic.ttf'
+  }
+}
+
+const PdfPrinter = require('pdfmake')
+const printer = new PdfPrinter(fonts)
+const fs = require('fs')
+
+const docDefinition = {
+  content: [
+    'First paragraph',
+    'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
+  ]
+}
+
+const pdfDoc = printer.createPdfKitDocument(docDefinition)
+pdfDoc.pipe(fs.createWriteStream('pdfs/basics.pdf'))
+pdfDoc.end()
