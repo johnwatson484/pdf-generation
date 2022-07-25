98ebd14ea5bdb32fab9e89f01a63b31755444493
John Watson <johnwatson484@hotmail.com>
2022-07-25 16:43:09 +0100

    Add part 3

--- app/generate-content/part2.js
+++ app/generate-content/part2.js
@@ -1,7 +1,8 @@
 const part2 = (statement) => {
   return [
     title,
-    getSummaryText(statement.scheme)
+    getSummaryText(statement.scheme),
+    getTable(statement.funding)
   ]
 }
 
@@ -14,6 +15,27 @@ const getSummaryText = (scheme) => {
   return {
     text: `We calculated the total ${scheme.shortName} payment amount by adding together payments for all the standards which are part of your agreement, as shown in this table.`
   }
+}
+
+const getTable = (funding) => {
+  return {
+    layout: {
+      hLineStyle: () => 'solid',
+      vLineStyle: () => 'solid'
+    },
+    table: {
+      headerRows: 1,
+      widths: ['*', '*', '*', '*', '*', '*', '*'],
+      body: [
+        ['Standard', 'Level', 'Land area (ha)', 'Annual value', 'Quarterly value', 'Quarterly reduction', 'Quarterly reduction'],
+        ...mapFundingRows(funding)
+      ]
+    }
+  }
+}
+
+const mapFundingRows = (funding) => {
+  return funding.map(x => ([x.name, x.level, x.area, x.annualValue, x.quarterlyValue, x.quarterlyReduction, x.quarterlyPayment]))
 }
 
 module.exports = part2
